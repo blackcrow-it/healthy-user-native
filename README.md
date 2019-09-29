966e54600f52e7cf28ea8b366d68fe12596b7155
hungnq <quanghungleo@gmail.com>
2019-09-29 23:13:34 +0700

    Add: Thêm page info

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # healthy-user-native
+
+------------ install env   ------------
+
+	npm install -g @angular/cli
+    npm install -g ionic
+
+------------ run app   ------------
+
+	ionic serve
--- src/app/first/info/info.page.ts
+++ src/app/first/info/info.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+import { PickerOptions } from '@ionic/core';
 
 @Component({
   selector: 'app-info',
@@ -6,10 +8,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info.page.scss'],
 })
 export class InfoPage implements OnInit {
+  weight = '';
+  height = '';
+  arrayNatural = [];
+  arrayDecimal = [];
+  arrayHeight = [];
+  optsWeight: PickerOptions = {
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      },
+      {
+        text: 'Done'
+      }
+    ],
+    columns: [
+      {
+        name: 'natural',
+        options: this.arrayNatural
+      },
+      {
+        name: 'decimal',
+        options: this.arrayDecimal
+      },
+      {
+        name: 'unit',
+        options: [
+          { text : 'kg', value : ''}
+        ]
+      }
+    ]
+  };
 
-  constructor() { }
+  optsHeight: PickerOptions = {
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      },
+      {
+        text: 'Done'
+      }
+    ],
+    columns: [
+      {
+        name: 'height',
+        options: this.arrayHeight
+      },
+      {
+        name: 'unit',
+        options: [
+          { text : ' cm', value : ''}
+        ]
+      }
+    ]
+  };
+
+  constructor(private pickerController: PickerController) { }
 
   ngOnInit() {
+    for (let index = 0; index <= 300; index++) {
+      let dictWeight = { text: index, value: index };
+      this.arrayNatural.push(dictWeight);
+      this.arrayHeight.push(dictWeight);
+    }
+    for (let index = 0; index <= 9; index++) {
+      let dictWeight = { text: "." + index, value: index };
+      this.arrayDecimal.push(dictWeight);
+    }
+  }
+
+  async showWeightPicker() {
+    let picker = await this.pickerController.create(this.optsWeight);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let colNatural = await picker.getColumn('natural');
+      let colDecimal = await picker.getColumn('decimal');
+      this.weight = colNatural.options[colNatural.selectedIndex].text + colDecimal.options[colDecimal.selectedIndex].text + " kg";
+    });
+  }
+
+  async showHeightPicker() {
+    let picker = await this.pickerController.create(this.optsHeight);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let col = await picker.getColumn('height');
+      this.height = col.options[col.selectedIndex].text + " cm";
+    });
   }
 
 }
